4b4b31fe86fd01ffbd38931971c6aafb42246a9f
Kevin Lee <kevinlee@Kevins-MacBook-Pro-3.local>
2022-07-25 12:51:09 -0700

    using gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,21 @@ module.exports = {
   },
   plugins: [`react-helmet`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-1T28FFB775", 
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-1T28FFB775", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
   ],
